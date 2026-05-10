0de8d117da7a657045ef640b3a0e74db4dc0d994
pandita1589 <fabian1234andre@gmail.com>
2026-05-10 14:01:25 -0500

    moon Studios v1.0.2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-tauri::Builder::default()
-    .plugin(tauri_plugin_updater::Builder::new().build())
-    .plugin(tauri_plugin_process::init())
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  app_lib::run();
+    tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
